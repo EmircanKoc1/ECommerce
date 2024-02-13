eedbed9eb56715e103281bd2e1c52659eedeb400
Emircan <emircandv787@gmail.com>
2024-02-13 18:29:15 +0300

    some services added

--- BusinessLogicLayer/Services/Concretes/ProductFeatureService.cs
+++ BusinessLogicLayer/Services/Concretes/ProductFeatureService.cs
@@ -1,97 +1,230 @@
-﻿using BusinessLogicLayer.Services.Abstracts;
+﻿using AutoMapper;
+using BusinessLogicLayer.Extensions;
+using BusinessLogicLayer.Services.Abstracts;
 using CoreLayer.DTOs;
 using CoreLayer.Enums;
 using CoreLayer.Model;
 using DataAccessLayer.Entities;
+using DataAccessLayer.Repositories.Abstracts;
 using System.Linq.Expressions;
 
 namespace BusinessLogicLayer.Services.Concretes
 {
     public class ProductFeatureService : IProductFeatureService
     {
-        public ProductFeatureDto Add(ProductFeatureDto dto)
+        IProductFeatureRepository _repository;
+        IMapper _mapper;
+
+        public ProductFeatureService(IProductFeatureRepository repository, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _repository = repository;
+            _mapper = mapper;
         }
 
-        public Task<ProductFeatureDto> AddAsync(ProductFeatureDto dto)
+        void CustomValidator(ProductFeatureDto dto)
         {
-            throw new NotImplementedException();
+            dto.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            if (dto.ProductId is null or 0)
+                dto.ThrowIfNull("", CustomException.ForeignKeyOrNavigationPropertyNullException);
+        }
+        public ProductFeatureDto Add(ProductFeatureDto dto)
+        {
+            CustomValidator(dto);
+
+            var entity = _mapper.Map<ProductFeature>(dto);
+
+            _repository.Add(entity);
+            _repository.SaveChanges();
+
+            return _mapper.Map<ProductFeatureDto>(entity);
+        }
+
+        public async Task<ProductFeatureDto> AddAsync(ProductFeatureDto dto)
+        {
+            CustomValidator(dto);
+
+            var entity = _mapper.Map<ProductFeature>(dto);
+
+            await _repository.AddAsync(entity);
+            await _repository.SaveChangesAsync();
+
+            return _mapper.Map<ProductFeatureDto>(entity);
         }
 
         public IEnumerable<ProductFeatureDto> AddRange(IEnumerable<ProductFeatureDto> dtos)
         {
-            throw new NotImplementedException();
+            dtos.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            foreach (var dto in dtos)
+                CustomValidator(dto);
+
+            var entities = _mapper.Map<IEnumerable<ProductFeature>>(dtos);
+            _repository.AddRange(entities);
+            _repository.SaveChanges();
+
+
+            return _mapper.Map<IEnumerable<ProductFeatureDto>>(entities);
         }
 
-        public Task<IEnumerable<ProductFeatureDto>> AddRangeAsync(IEnumerable<ProductFeatureDto> dtos)
+        public async Task<IEnumerable<ProductFeatureDto>> AddRangeAsync(IEnumerable<ProductFeatureDto> dtos)
         {
-            throw new NotImplementedException();
+            dtos.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            foreach (var dto in dtos)
+                CustomValidator(dto);
+
+            var entities = _mapper.Map<IEnumerable<ProductFeature>>(dtos);
+
+            await _repository.AddRangeAsync(entities);
+            await _repository.SaveChangesAsync();
+
+            return _mapper.Map<IEnumerable<ProductFeatureDto>>(entities);
+
         }
 
         public ProductFeatureDto Delete(long id)
         {
-            throw new NotImplementedException();
+            var entity = _repository.GetById(false, id);
+
+            entity.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            _repository.Delete(entity);
+            _repository.SaveChanges();
+
+            return _mapper.Map<ProductFeatureDto>(entity);
+
         }
 
-        public ProductFeatureDto Delete(ProductFeatureDto dtos)
+        public ProductFeatureDto Delete(ProductFeatureDto dto)
         {
-            throw new NotImplementedException();
+            dto.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            var entity = _repository.GetById(false, dto.Id);
+
+            entity.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            _repository.Delete(entity);
+            _repository.SaveChanges();
+
+            return _mapper.Map<ProductFeatureDto>(entity);
         }
 
-        public Task<ProductFeatureDto> DeleteAsync(long id)
+        public async Task<ProductFeatureDto> DeleteAsync(long id)
         {
-            throw new NotImplementedException();
+            var entity = await _repository.GetByIdAsync(false, id);
+
+            entity.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            _repository.Delete(entity);
+            _repository.SaveChanges();
+
+            return _mapper.Map<ProductFeatureDto>(entity);
         }
 
-        public IEnumerable<ProductFeatureDto> DeleteRange(IEnumerable<ProductFeatureDto> dtos)
+        public void DeleteRange(IEnumerable<ProductFeatureDto> dtos)
         {
-            throw new NotImplementedException();
+            dtos.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            foreach (var dto in dtos)
+                dto.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            var entities = _mapper.Map<IEnumerable<ProductFeature>>(dtos);
+            _repository.DeleteRange(entities);
+            _repository.SaveChanges();
+
         }
 
-        public IEnumerable<ProductFeatureDto> DeleteRange(Expression<Func<ProductFeature, bool>> predicate)
+        public void DeleteRange(Expression<Func<ProductFeature, bool>> predicate)
         {
-            throw new NotImplementedException();
+            predicate.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            _repository.DeleteRange(predicate);
+            _repository.SaveChanges();
+
         }
 
         public IEnumerable<ProductFeatureDto> GetAll(PaginationModel paginationModel, bool tracking = false)
         {
-            throw new NotImplementedException();
+            var entities = _repository.GetAll(paginationModel, tracking);
+            entities.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            return _mapper.Map<IEnumerable<ProductFeatureDto>>(entities);
         }
 
         public IEnumerable<ProductFeatureDto> GetAll(PaginationModel paginationModel, Expression<Func<ProductFeature, bool>> predicate, Expression<Func<ProductFeature, long>> orderByKeySelector, OrderByDirection direction, bool tracking = false, params Expression<Func<ProductFeature, long>>[] thenByKeySelector)
         {
-            throw new NotImplementedException();
+            var entities = _repository.GetAll(paginationModel, predicate, orderByKeySelector, direction, tracking, thenByKeySelector);
+
+            entities.ThrowIfNull("", CustomException.EntityNotFoundException);
+            return _mapper.Map<IEnumerable<ProductFeatureDto>>(entities);
+
+
         }
 
-        public Task<IEnumerable<ProductFeatureDto>> GetAllAsync(PaginationModel paginationModel, Expression<Func<ProductFeature, bool>> predicate, Expression<Func<ProductFeature, long>> orderByKeySelector, OrderByDirection direction, bool tracking = false, params Expression<Func<ProductFeature, long>>[] thenByKeySelector)
+        public async Task<IEnumerable<ProductFeatureDto>> GetAllAsync(PaginationModel paginationModel, Expression<Func<ProductFeature, bool>> predicate, Expression<Func<ProductFeature, long>> orderByKeySelector, OrderByDirection direction, bool tracking = false, params Expression<Func<ProductFeature, long>>[] thenByKeySelector)
         {
-            throw new NotImplementedException();
+            var entities = await _repository.GetAllAsync(paginationModel, predicate, orderByKeySelector, direction, tracking, thenByKeySelector);
+
+            entities.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            return _mapper.Map<IEnumerable<ProductFeatureDto>>(entities);
+
         }
 
         public ProductFeatureDto GetById(bool tracking = false, params object[] keyValues)
         {
-            throw new NotImplementedException();
+            var entity = _repository.GetById(tracking, keyValues);
+            entity.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            return _mapper.Map<ProductFeatureDto>(entity);
+
         }
 
-        public Task<ProductFeatureDto> GetByIdAsync(bool tracking = false, params object[] keyValues)
+        public async Task<ProductFeatureDto> GetByIdAsync(bool tracking = false, params object[] keyValues)
         {
-            throw new NotImplementedException();
+            var entity = await _repository.GetByIdAsync(tracking, keyValues);
+            entity.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            return _mapper.Map<ProductFeatureDto>(entity);
         }
 
         public ProductFeatureDto GetFirstOrDefault(Expression<Func<ProductFeature, bool>> predicate, bool tracking = false)
         {
-            throw new NotImplementedException();
+            var entity = _repository.GetFirstOrDefault(predicate, tracking);
+
+            entity.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            return _mapper.Map<ProductFeatureDto>(entity);
+
         }
 
-        public Task<ProductFeatureDto> GetFirstOrDefaultAsync(Expression<Func<ProductFeature, bool>> predicate, bool tracking = false)
+        public async Task<ProductFeatureDto> GetFirstOrDefaultAsync(Expression<Func<ProductFeature, bool>> predicate, bool tracking = false)
         {
-            throw new NotImplementedException();
+            var entity = await _repository.GetFirstOrDefaultAsync(predicate, tracking);
+
+            entity.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            return _mapper.Map<ProductFeatureDto>(entity);
         }
 
-        public (ProductFeatureDto, ProductFeatureDto) Update(ProductFeatureDto dtos)
+        public (ProductFeatureDto, ProductFeatureDto) Update(ProductFeatureDto dto)
         {
-            throw new NotImplementedException();
+            dto.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            var foundedEntity = _repository.GetById(false, dto.Id);
+
+            foundedEntity.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            var oldEntity = _mapper.Map<ProductFeatureDto>(foundedEntity);
+            var newEntity = _mapper.Map<ProductFeature>(dto);
+
+            _repository.Update(newEntity);
+            _repository.SaveChanges();
+
+            return (oldEntity, dto);
+
+
         }
     }
 
--- BusinessLogicLayer/Services/Concretes/TagService.cs
+++ BusinessLogicLayer/Services/Concretes/TagService.cs
@@ -1,97 +1,209 @@
-﻿using BusinessLogicLayer.Services.Abstracts;
+﻿using AutoMapper;
+using BusinessLogicLayer.Extensions;
+using BusinessLogicLayer.Services.Abstracts;
 using CoreLayer.DTOs;
 using CoreLayer.Enums;
 using CoreLayer.Model;
 using DataAccessLayer.Entities;
+using DataAccessLayer.Repositories.Abstracts;
 using System.Linq.Expressions;
 
 namespace BusinessLogicLayer.Services.Concretes
 {
     public class TagService : ITagService
     {
-        public TagDto Add(TagDto dto)
+        IMapper _mapper;
+        ITagRepository _repository;
+
+        public TagService(IMapper mapper, ITagRepository repository)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
+            _repository = repository;
         }
 
-        public Task<TagDto> AddAsync(TagDto dto)
+        public TagDto Add(TagDto dto)
         {
-            throw new NotImplementedException();
+            dto.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            var entity = _mapper.Map<Tag>(dto);
+            _repository.Add(entity);
+            _repository.SaveChanges();
+
+            return _mapper.Map<TagDto>(entity);
+        }
+
+        public async Task<TagDto> AddAsync(TagDto dto)
+        {
+            dto.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            var entity = _mapper.Map<Tag>(dto);
+            await _repository.AddAsync(entity);
+            await _repository.SaveChangesAsync();
+
+            return _mapper.Map<TagDto>(entity);
         }
 
         public IEnumerable<TagDto> AddRange(IEnumerable<TagDto> dtos)
         {
-            throw new NotImplementedException();
+            dtos.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            foreach (var dto in dtos)
+                dto.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            var entities = _mapper.Map<IEnumerable<Tag>>(dtos);
+            _repository.AddRange(entities);
+            _repository.SaveChanges();
+
+            return _mapper.Map<IEnumerable<TagDto>>(entities);
         }
 
-        public Task<IEnumerable<TagDto>> AddRangeAsync(IEnumerable<TagDto> dtos)
+        public async Task<IEnumerable<TagDto>> AddRangeAsync(IEnumerable<TagDto> dtos)
         {
-            throw new NotImplementedException();
+            dtos.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            foreach (var dto in dtos)
+                dto.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            var entities = _mapper.Map<IEnumerable<Tag>>(dtos);
+            await _repository.AddRangeAsync(entities);
+            await _repository.SaveChangesAsync();
+
+            return _mapper.Map<IEnumerable<TagDto>>(entities);
         }
 
         public TagDto Delete(long id)
         {
-            throw new NotImplementedException();
+            var entity = _repository.GetById(false, id);
+
+            entity.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            _repository.Delete(entity);
+            _repository.SaveChanges();
+
+            return _mapper.Map<TagDto>(entity);
         }
 
-        public TagDto Delete(TagDto dtos)
+        public TagDto Delete(TagDto dto)
         {
-            throw new NotImplementedException();
+            var entity = _repository.GetById(false, dto.Id);
+
+            entity.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            _repository.Delete(entity);
+            _repository.SaveChanges();
+
+            return _mapper.Map<TagDto>(entity);
         }
 
-        public Task<TagDto> DeleteAsync(long id)
+        public async Task<TagDto> DeleteAsync(long id)
         {
-            throw new NotImplementedException();
+            var entity = await _repository.GetByIdAsync(false, id);
+
+            entity.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            _repository.Delete(entity);
+            _repository.SaveChanges();
+
+            return _mapper.Map<TagDto>(entity);
         }
 
-        public IEnumerable<TagDto> DeleteRange(IEnumerable<TagDto> dtos)
+        public void DeleteRange(IEnumerable<TagDto> dtos)
         {
-            throw new NotImplementedException();
+            dtos.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            foreach (var dto in dtos)
+                dto.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            var entities = _mapper.Map<IEnumerable<Tag>>(dtos);
+
+            _repository.DeleteRange(entities);
+            _repository.SaveChanges();
+
         }
 
-        public IEnumerable<TagDto> DeleteRange(Expression<Func<Tag, bool>> predicate)
+        public void DeleteRange(Expression<Func<Tag, bool>> predicate)
         {
-            throw new NotImplementedException();
+            predicate.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            _repository.DeleteRange(predicate);
+            _repository.SaveChanges();
         }
 
         public IEnumerable<TagDto> GetAll(PaginationModel paginationModel, bool tracking = false)
         {
-            throw new NotImplementedException();
+            var entities = _repository.GetAll(paginationModel, tracking);
+
+            entities.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            return _mapper.Map<IEnumerable<TagDto>>(entities);
+
         }
 
         public IEnumerable<TagDto> GetAll(PaginationModel paginationModel, Expression<Func<Tag, bool>> predicate, Expression<Func<Tag, long>> orderByKeySelector, OrderByDirection direction, bool tracking = false, params Expression<Func<Tag, long>>[] thenByKeySelector)
         {
-            throw new NotImplementedException();
+            var entities = _repository.GetAll(paginationModel, predicate, orderByKeySelector, direction, tracking, thenByKeySelector);
+
+            entities.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            return _mapper.Map<IEnumerable<TagDto>>(entities);
         }
 
-        public Task<IEnumerable<TagDto>> GetAllAsync(PaginationModel paginationModel, Expression<Func<Tag, bool>> predicate, Expression<Func<Tag, long>> orderByKeySelector, OrderByDirection direction, bool tracking = false, params Expression<Func<Tag, long>>[] thenByKeySelector)
+        public async Task<IEnumerable<TagDto>> GetAllAsync(PaginationModel paginationModel, Expression<Func<Tag, bool>> predicate, Expression<Func<Tag, long>> orderByKeySelector, OrderByDirection direction, bool tracking = false, params Expression<Func<Tag, long>>[] thenByKeySelector)
         {
-            throw new NotImplementedException();
+            var entities = await _repository.GetAllAsync(paginationModel, predicate, orderByKeySelector, direction, tracking, thenByKeySelector);
+
+            entities.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            return _mapper.Map<IEnumerable<TagDto>>(entities);
         }
 
         public TagDto GetById(bool tracking = false, params object[] keyValues)
         {
-            throw new NotImplementedException();
+            var entity = _repository.GetById(tracking, keyValues);
+            entity.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            return _mapper.Map<TagDto>(entity);
         }
 
-        public Task<TagDto> GetByIdAsync(bool tracking = false, params object[] keyValues)
+        public async Task<TagDto> GetByIdAsync(bool tracking = false, params object[] keyValues)
         {
-            throw new NotImplementedException();
+            var entity = await _repository.GetByIdAsync(tracking, keyValues);
+            entity.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            return _mapper.Map<TagDto>(entity);
         }
 
         public TagDto GetFirstOrDefault(Expression<Func<Tag, bool>> predicate, bool tracking = false)
         {
-            throw new NotImplementedException();
+            var entity = _repository.GetFirstOrDefault(predicate, tracking);
+            entity.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            return _mapper.Map<TagDto>(entity);
         }
 
-        public Task<TagDto> GetFirstOrDefaultAsync(Expression<Func<Tag, bool>> predicate, bool tracking = false)
+        public async Task<TagDto> GetFirstOrDefaultAsync(Expression<Func<Tag, bool>> predicate, bool tracking = false)
         {
-            throw new NotImplementedException();
+            var entity = await _repository.GetFirstOrDefaultAsync(predicate, tracking);
+            entity.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            return _mapper.Map<TagDto>(entity);
         }
 
-        public (TagDto, TagDto) Update(TagDto dtos)
+        public (TagDto, TagDto) Update(TagDto dto)
         {
-            throw new NotImplementedException();
+            dto.ThrowIfNull("", CustomException.ParameterValueNullException);
+
+            var foundedEntity = _repository.GetById(false, dto.Id);
+
+            foundedEntity.ThrowIfNull("", CustomException.EntityNotFoundException);
+
+            var oldEntity = _mapper.Map<TagDto>(foundedEntity);
+            var newEntity = _mapper.Map<Tag>(dto);
+
+            _repository.Update(newEntity);
+            _repository.SaveChanges();
+
+            return (oldEntity, dto);
         }
     }
 
