2674f8992182e80610c267e0742e4d4158b7f603
Emircan <emircandv787@gmail.com>
2024-04-08 00:29:31 +0300

    Controller classes added

---
+++ Presentation.WebAPI/Controllers/PaymentCardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentCardController : ControllerBase
+    {
+    }
+}
